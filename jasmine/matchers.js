84ac86867643d06fc4143b65e24b25b34bf622f6
Dave Batiste <dave.batiste@d2l.com>
2014-09-18 17:27:51 -0400

    Namespacing the custom jasmine matchers.

--- jasmine/matchers.js
+++ jasmine/matchers.js
@@ -1,34 +1,42 @@
-var customMatchers = {
+var d2l = {
 
-	toHaveCssSelector: function() {
-		return {
-			compare: function( actual, expected ) {
+	jasmine: {
 
-			if ( !actual ) {
-				return { pass: false, message: 'The actual value is not defined.' };
-			}
+		matchers: {
 
-			if ( !expected ) {
-				return { pass: false, message: 'The expected value is not defined.' };
-			}
+			toHaveCssSelector: function() {
+				return {
+					compare: function( actual, expected ) {
 
-			var pattern = new RegExp( expected );
+					if ( !actual ) {
+						return { pass: false, message: 'The actual value is not defined.' };
+					}
 
-			for ( var i=0; i<document.styleSheets.length; ++i ) {
-				for ( var j=0; j<document.styleSheets[i].cssRules.length; ++j ) {
-					var selectors = document.styleSheets[i].cssRules[j].selectorText.split( ',' );
-					for ( var k=0; k<selectors.length; k++ ) {
-						if ( pattern.test( selectors[k] ) ) {
-							return { pass: true };
+					if ( !expected ) {
+						return { pass: false, message: 'The expected value is not defined.' };
+					}
+
+					var pattern = new RegExp( expected );
+
+					for ( var i=0; i<document.styleSheets.length; ++i ) {
+						for ( var j=0; j<document.styleSheets[i].cssRules.length; ++j ) {
+							var selectors = document.styleSheets[i].cssRules[j].selectorText.split( ',' );
+							for ( var k=0; k<selectors.length; k++ ) {
+								if ( pattern.test( selectors[k] ) ) {
+									return { pass: true };
+								}
+							}
 						}
 					}
-				}
+
+						return { pass: false, message: 'Expected to find ' + expected + ' css selector but did not.' };
+
+					}
+				};
 			}
 
-				return { pass: false, message: 'Expected to find ' + expected + ' css selector but did not.' };
+		}
 
-			}
-		};
 	}
 
 };
