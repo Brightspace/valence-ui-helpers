69542f4af293303a551bdc2de2784c12d352e711
Dave Batiste <dave.batiste@d2l.com>
2014-09-18 17:34:40 -0400

    Look for exact selector match when evaluating the CSS selector matcher.

--- jasmine/matchers.js
+++ jasmine/matchers.js
@@ -16,13 +16,11 @@ var d2l = {
 						return { pass: false, message: 'The expected value is not defined.' };
 					}
 
-					var pattern = new RegExp( expected );
-
 					for ( var i=0; i<document.styleSheets.length; ++i ) {
 						for ( var j=0; j<document.styleSheets[i].cssRules.length; ++j ) {
 							var selectors = document.styleSheets[i].cssRules[j].selectorText.split( ',' );
 							for ( var k=0; k<selectors.length; k++ ) {
-								if ( pattern.test( selectors[k] ) ) {
+								if ( selectors[k].replace( /(^\s+|\s+$)/g,'' ) === expected ) {
 									return { pass: true };
 								}
 							}
