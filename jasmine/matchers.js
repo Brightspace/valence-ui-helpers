c8748c0f7df28d9778f824c90d41fa829aaddbfd
Dave Batiste <dave.batiste@d2l.com>
2014-09-19 14:14:57 -0400

    Updating matcher to use the doc passed to it.

--- jasmine/matchers.js
+++ jasmine/matchers.js
@@ -6,26 +6,26 @@ var d2l = {
 
 			toHaveCssSelector: function() {
 				return {
-					compare: function( actual, expected ) {
+					compare: function( doc, expected ) {
 
-					if ( !actual ) {
-						return { pass: false, message: 'The actual value is not defined.' };
-					}
+						if ( !doc ) {
+							return { pass: false, message: 'The actual document is not defined.' };
+						}
 
-					if ( !expected ) {
-						return { pass: false, message: 'The expected value is not defined.' };
-					}
+						if ( !expected ) {
+							return { pass: false, message: 'The expected value is not defined.' };
+						}
 
-					for ( var i=0; i<document.styleSheets.length; ++i ) {
-						for ( var j=0; j<document.styleSheets[i].cssRules.length; ++j ) {
-							var selectors = document.styleSheets[i].cssRules[j].selectorText.split( ',' );
-							for ( var k=0; k<selectors.length; k++ ) {
-								if ( selectors[k].replace( /(^\s+|\s+$)/g,'' ) === expected ) {
-									return { pass: true };
+						for ( var i=0; i<doc.styleSheets.length; ++i ) {
+							for ( var j=0; j<doc.styleSheets[i].cssRules.length; ++j ) {
+								var selectors = doc.styleSheets[i].cssRules[j].selectorText.split( ',' );
+								for ( var k=0; k<selectors.length; k++ ) {
+									if ( selectors[k].replace( /(^\s+|\s+$)/g,'' ) === expected ) {
+										return { pass: true };
+									}
 								}
 							}
 						}
-					}
 
 						return { pass: false, message: 'Expected to find ' + expected + ' css selector but did not.' };
 
