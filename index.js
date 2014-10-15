12cc74983c4f917eba74eec5134447bcf2074729
Dave Batiste <dave.batiste@d2l.com>
2014-10-15 12:46:30 -0400

    Updating test helper to take config object.   Bumping version accordingly (breaking change).

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 var vfs = require( 'vinyl-fs' );
 
-var prefixOpts = { 
-	browsers: [ 
-		'last 4 Chrome versions', 
-		'last 4 Firefox versions', 
-		'Firefox ESR', 'IE >= 9', 
-		'Safari >= 5.1', 
-		'last 4 Opera versions' 
+var prefixOpts = {
+	browsers: [
+		'last 4 Chrome versions',
+		'last 4 Firefox versions',
+		'Firefox ESR', 'IE >= 9',
+		'Safari >= 5.1',
+		'last 4 Opera versions'
 	]
 };
 
@@ -36,28 +36,52 @@ var makeLess = function( source, target ) {
 
 };
 
-var test = function( config, js, css ) {
+var test = function( config ) {
 
-	var files = [ 'node_modules/vui-helpers/jasmine/matchers.js' ];
-
-	if ( typeof js === 'string' ) {
-		files.push( js );
-	} else if ( ( typeof js === 'object' ) && js.length !== undefined ) {
-		files = files.concat( js );
+	if ( !config ) {
+		console.log( 'No config specified.' );
+		return;
 	}
 
-	if ( css !== undefined ) {
-		files.push( { pattern: css, included: true } );
+	var karmaConfig = {
+		configFile: 'node_modules/vui-helpers/karma.conf.js',
+		files: [ 'node_modules/vui-helpers/jasmine/matchers.js' ],
+		action: 'run'
+	};
+
+	for( var key in config ) {
+		if ( key === 'files' ) {
+
+			// merge files
+			for( var i=0; i<config.files.length; i++ ) {
+				var file = config.files[i];
+				if ( typeof file === 'string' ) {
+					var ext = file.substr( file.lastIndexOf( '.' ) );
+					if ( ext && ext.toLowerCase() === '.css' ) {
+
+						// karma requires that css files be explicitly included
+						karmaConfig.files.push( { pattern: file, included: true } );
+
+					} else {
+						karmaConfig.files.push( file );
+					}
+				} else if ( typeof file === 'object' ) {
+					karmaConfig.files.push( file );
+				}
+			}
+
+		} else {
+
+			// copy forward other configs
+			karmaConfig[ key ] = config[ key ];
+
+		}
 	}
 
 	var karma = require( 'gulp-karma' );
 
 	return vfs.src( 'empty-stream' )
-		.pipe( karma( { 
-			configFile: config, 
-			files: files,
-			action: 'run' 
-		} ) );
+		.pipe( karma( karmaConfig ) );
 
 };
 
